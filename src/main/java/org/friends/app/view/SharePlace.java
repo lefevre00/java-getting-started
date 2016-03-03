0af860a5e2cadcec6238d08c3aa3ce17f7cb33a9
vwilliam <william.verdeil@gmail.com>
2016-03-03 21:35:11 +0100

    Correction redirection

--- src/main/java/org/friends/app/view/SharePlace.java
+++ src/main/java/org/friends/app/view/SharePlace.java
@@ -1,12 +1,6 @@
 package org.friends.app.view;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.friends.app.model.Place;
@@ -37,9 +31,6 @@ public class SharePlace implements TemplateViewRoute {
 			if ("POST".equalsIgnoreCase(paramRequest.requestMethod())) {
 					if(paramRequest.queryParams("dateDebut") != null){
 						placeService.releasePlace(user.getPlaceNumber().intValue(), paramRequest.queryParams("dateDebut"));
-						map.put("numberPlace", paramRequest.queryParams("number"));
-						map.put("places", getPlaces());
-				        model = new ModelAndView(map, "search.ftl");
 				        paramResponse.redirect("/protected/search");
 					}
 			}else{
@@ -49,15 +40,6 @@ public class SharePlace implements TemplateViewRoute {
 		}
 		
 		return model;
-	}
-	
-	private List<Place> getPlaces() throws ParseException {
-		List<Place> places = new ArrayList<>();
-		List<Integer> freePlaces = placeService.getAvailableByDate(new Date());
-		for (int i = 1; i<150; i++) {
-			places.add(new Place(i, freePlaces.contains(i)));
-		}
-		return places;
 	}
 	
 }
