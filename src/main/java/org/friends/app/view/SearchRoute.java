222ff5890233b5ff635c05e0788b99bfaa99c339
vwilliam <william.verdeil@gmail.com>
2016-03-04 11:25:53 +0100

    Test Commit 2...

--- src/main/java/org/friends/app/view/SearchRoute.java
+++ src/main/java/org/friends/app/view/SearchRoute.java
@@ -3,7 +3,9 @@ package org.friends.app.view;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -25,20 +27,30 @@ public class SearchRoute implements TemplateViewRoute {
 	public ModelAndView handle(Request req, Response resp) throws Exception {
     	Map<String, Object> map = new HashMap<>();
     	Date dateRecherche = new Date();
+    	
     	String strdateRecherche = new SimpleDateFormat("dd/MM/yyyy").format(new Date(dateRecherche.getTime()));
-    	String strTomorrow = new SimpleDateFormat("dd/MM/yyyy").format(new Date(dateRecherche.getTime() + (1000 * 60 * 60 * 24)));
-    	//if ("POST".equalsIgnoreCase(req.requestMethod())) {
+    	String strTomorrow = rechercherLejourSuivant(dateRecherche);
+//    	//if ("POST".equalsIgnoreCase(req.requestMethod())) {
     		if(req.queryParams("nextDay") != null){
     			dateRecherche = strtoDate(req.queryParams("nextDay"));
     			strdateRecherche = new SimpleDateFormat("dd/MM/yyyy").format(new Date(dateRecherche.getTime()));
-    			strTomorrow = new SimpleDateFormat("dd/MM/yyyy").format(new Date(dateRecherche.getTime() + (1000 * 60 * 60 * 24)));
-    			map.put("nextDay", "?nextDay="+strTomorrow);
-	    	}else{
-	    		map.put("nextDay", "?nextDay="+strTomorrow);
+    			strTomorrow = rechercherLejourSuivant(dateRecherche);
 	    	}
+    		map.put("nextDay", "?nextDay="+strTomorrow);
     		map.put("dateRecherche", strdateRecherche);
     	map.put("places", getPlaces(dateRecherche));
         return new ModelAndView(map, "search.ftl");
+	}
+
+	private String rechercherLejourSuivant(Date dateRecherche) {
+		int nextDay = 1;
+		Calendar calendar = new GregorianCalendar();
+	    calendar.setTime(dateRecherche);
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        if(dayOfWeek == Calendar.FRIDAY){
+			nextDay = 3;
+		}
+		return new SimpleDateFormat("dd/MM/yyyy").format(new Date(dateRecherche.getTime() + (1000 * 60 * 60 * 24 * nextDay)));
 	}
 
 	private List<Place> getPlaces(Date dateRecherche) throws ParseException {
