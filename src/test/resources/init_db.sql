4ca07ef3e6f6f64441b7458846e12368f8167d66
Michaël Lefèvre <lefevre00@yahoo.fr>
2016-03-11 16:58:01 +0100

    More tables

--- src/test/resources/init_db.sql
+++ src/test/resources/init_db.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users
 (
   id serial NOT NULL,
@@ -6,5 +5,28 @@ CREATE TABLE users
   place integer,
   password character varying(50) NOT NULL,
   token character varying(100),
-  CONSTRAINT prim_key_users PRIMARY KEY (id)
+  CONSTRAINT prim_key_users PRIMARY KEY (id),
+  CONSTRAINT uniq_email UNIQUE (email)
+)
+
+CREATE TABLE places
+(
+  place_id integer NOT NULL,
+  mail_occupant character varying(255),
+  occupation_date character varying(10) NOT NULL,
+  CONSTRAINT places_pkey PRIMARY KEY (place_id, occupation_date),
+  CONSTRAINT user_fkey FOREIGN KEY (mail_occupant)
+      REFERENCES users (email) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+CREATE TABLE sessions
+(
+  user_id integer NOT NULL,
+  creation_date timestamp without time zone NOT NULL DEFAULT now(),
+  cookie character varying(100) NOT NULL,
+  CONSTRAINT pkey_session PRIMARY KEY (cookie),
+  CONSTRAINT fkey_user_id FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
