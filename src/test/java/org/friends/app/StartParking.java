317a8df5543b2e0e372d2f2a065b14017096a2fa
vwilliam <william.verdeil@gmail.com>
2016-03-12 22:37:05 +0100

    Ajout date expiration dans table session

--- src/test/java/org/friends/app/StartParking.java
+++ src/test/java/org/friends/app/StartParking.java
@@ -36,12 +36,13 @@ public class StartParking {
 				super.start();
 				Connection connexion = getConnection();
 				Statement stmt = connexion.createStatement();
+				// A décommenter quand on touche la structure d'une des 3 tables
 				//stmt.executeUpdate("DROP TABLE users");
 				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL, email VARCHAR(255) NOT NULL, place_id INT, pwd varchar(255) NOT NULL, token varchar(100), PRIMARY KEY (id))");
 				//stmt.executeUpdate("DROP TABLE places");
 				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS places (id int NOT NULL, email_occupant varchar(255), occupation_date varchar(10) NOT NULL, PRIMARY KEY (id, occupation_date))");
 				//stmt.executeUpdate("DROP TABLE sessions");
-				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS sessions (user_id int NOT NULL, creation_date TIMESTAMP NOT NULL DEFAULT NOW(), cookie character varying(100) NOT NULL, PRIMARY KEY (cookie))");
+				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS sessions (user_id int NOT NULL, creation_date TIMESTAMP NOT NULL DEFAULT NOW(), cookie character varying(100) NOT NULL, expiration_date timestamp NOT NULL,  PRIMARY KEY (cookie))");
 				stmt.executeUpdate("INSERT INTO USERS (id, email, place_id, pwd, token) SELECT 1, 'william.verdeil@amdm.fr', 141 , 'wv','token' WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 1 AND email = 'william.verdeil@amdm.fr')");
 				stmt.executeUpdate("INSERT INTO USERS (id, email, place_id, pwd, token) SELECT 2, 'abdel.tamditi@amdm.fr', 133 , 'at','token1' WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 2 AND email = 'abdel.tamditi@amdm.fr')");
 				stmt.executeUpdate("INSERT INTO USERS (id, email, place_id, pwd, token) SELECT 3, 'michael.lefevre@amdm.fr', 87 , 'ml','token2' WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 3 AND email = 'michael.lefevre@amdm.fr')");
