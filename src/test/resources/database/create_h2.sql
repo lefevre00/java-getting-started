223ec36dd99afdf61d0a1449531ae59afa743d92
Michaël Lefèvre <lefevre00@yahoo.fr>
2016-08-23 15:23:17 +0200

    Drop tables "IF EXISTS"

--- src/test/resources/database/create_h2.sql
+++ src/test/resources/database/create_h2.sql
@@ -1,6 +1,6 @@
-DROP TABLE places;
-DROP TABLE sessions;
-DROP TABLE users IF EXISTS;
+DROP TABLE IF EXISTS places;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS users;
 create table PLACES (OCCUPATION_DATE varchar(255) not null, ID integer not null, EMAIL_OCCUPANT varchar(255), primary key (OCCUPATION_DATE, ID));
 create table SESSIONS (SESSION_ID integer not null, COOKIE varchar(255), CREATION_DATE timestamp, EXPIRATION_DATE timestamp, USER_ID integer, primary key (SESSION_ID));
 create table USERS (ID integer not null, EMAIL varchar(255), APIKEY varchar(255), PLACE_ID integer, PASSWORD varchar(255), TOKEN_MAIL varchar(255), TOKEN_PASSWORD varchar(255), primary key (ID));
