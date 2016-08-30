c8daa64402bd56dd651fee5139cd031a9f2eea6b
Abdel TAMDITI <tamditi@gmail.com>
2016-08-30 13:30:06 +0200

    Exclure la ligne administrateur de la liste des utilisateurs.

--- src/main/java/org/friends/app/view/route/UsersListRoute.java
+++ src/main/java/org/friends/app/view/route/UsersListRoute.java
@@ -1,7 +1,10 @@
 package org.friends.app.view.route;
 
+import java.util.List;
 import java.util.Map;
 
+import org.friends.app.ConfHelper;
+import org.friends.app.model.User;
 import org.friends.app.service.UserService;
 import org.friends.app.view.Templates;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +23,16 @@ public class UsersListRoute extends AdminAuthRoute {
 	@Override
 	public ModelAndView doHandle(Request request, Response response) {
 		Map<String, Object> map = Routes.getMap(request);
-		map.put("usersList", userService.getAllUser());
+		List<User> allUsers = userService.getAllUser();
+		if (allUsers!=null){
+			for (User user : allUsers){
+				if (ConfHelper.ADMIN_MAIL.equals(user.getEmailAMDM())){
+					allUsers.remove(user);
+					break;
+				}
+			}
+		}
+		map.put("usersList", allUsers);
 		return new ModelAndView(map, Templates.USERS_LIST);
 	}
 }
