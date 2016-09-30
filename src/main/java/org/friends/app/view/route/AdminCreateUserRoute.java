d6310c4735c1a192a7b75308401d795e6c48eee6
vwilliam <william.verdeil@gmail.com>
2016-09-30 23:29:14 +0200

    #133 closed

--- src/main/java/org/friends/app/view/route/AdminCreateUserRoute.java
+++ src/main/java/org/friends/app/view/route/AdminCreateUserRoute.java
@@ -1,11 +1,12 @@
 package org.friends.app.view.route;
 
+import static org.friends.app.view.RequestHelper.getAppUrl;
+
 import java.util.Map;
 
 import org.friends.app.model.User;
 import org.friends.app.service.UserService;
 import org.friends.app.validator.EmailValidator;
-import org.friends.app.view.RequestHelper;
 import org.friends.app.view.Templates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -56,7 +57,7 @@ public class AdminCreateUserRoute extends AdminAuthRoute {
 				
 				
 					User nouvelUtilisateur = new User(email, "1", StringUtils.isEmpty(placeNumber) ? null : Integer.parseInt(placeNumber));
-					userService.create(nouvelUtilisateur,  RequestHelper.getAppUrl(request));
+					userService.create(nouvelUtilisateur,  getAppUrl(request));
 				
 			} catch (NumberFormatException e) {
 				map.put(Routes.KEY_ERROR, "Le numéro de place est un numéro !");
