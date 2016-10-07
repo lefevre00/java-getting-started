716d1d4abcd0e50e064a83cae4144460a22ffb22
atamd <tamditi@gmail.com>
2016-10-07 18:48:23 +0200

    Update UnshareException.java

--- src/main/java/org/friends/app/service/UnshareException.java
+++ src/main/java/org/friends/app/service/UnshareException.java
@@ -26,7 +26,6 @@ package org.friends.app.service;
 
 /**
  * L'utilisateur cherche a annuler le partage d'une place occupée.  
- * @author michael lefevre
  */
 public class UnshareException extends Exception {
 
